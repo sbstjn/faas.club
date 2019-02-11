b1582d4f1d0e266431907b9b15ba7df22db2c540
sbstjn <mail@sbstjn.com>
2019-02-11 14:57:33 +0100

    feat: Add Welcome

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,24 @@ import Card from '../components/card'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+const Welcome = styled.p`
+  font-weight: 600;
+  line-height: 18px;
+  font-family: 'Raleway', sans-serif;
+  color: #8d99ae;
+  letter-spacing: 0.02em;
+  font-size: 13px;
+  text-align: center;
+  max-width: 420px;
+  margin: 20px auto 0 auto;
+
+  & > a {
+    text-decoration: none;
+    color: #8d99ae;
+    font-weight: 700;
+  }
+`
+
 const List = styled.div`
   margin: 50px 0 50px; 0;
 `
@@ -17,7 +35,7 @@ const IndexPage = ({
 }) => (
   <Layout>
     <SEO title="FaaS.club" keywords={[`faas`, `faasclub`, `serverless`, `function as a service`, `lambda`]} />
-
+    <Welcome>Curated list of resources for all things Serverless.</Welcome>
     <List>
       {edges.map(edge => (
         <Card {...edge.node} />
